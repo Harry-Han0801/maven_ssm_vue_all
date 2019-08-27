9c5e31457a188e71cf121ae9c94376c04caa6605
小川 <956285095@qq.com>
2019-08-27 22:17:11 +0800

    update1

--- src/main/java/com/hwua/service/impl/UserServiceImpl.java
+++ src/main/java/com/hwua/service/impl/UserServiceImpl.java
@@ -19,4 +19,9 @@ public class UserServiceImpl implements UserService {
     public List<User> selectUser() {
         return userMapper.selectAll();
     }
+
+    @Override
+    public List<User> selectAllUser() {
+        return null;
+    }
 }
